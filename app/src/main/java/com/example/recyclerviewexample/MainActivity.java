e0e3de12b846c328ddcb92ec0f8ef28f98d78e4d
dudwns0911 <dudwns0911@naver.com>
2020-05-03 12:19:32 +0900

    recyclerview

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            list.add(String.format("안녕 %d", i));
+        }
+
+        RecyclerView recyclerView = findViewById(R.id.recycler1);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        MyAdapter adapter =new MyAdapter(list);
+        recyclerView.setAdapter(adapter);
     }
 }
