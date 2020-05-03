6efc93e7fe52d837efcb3ee4156418bcd6be81ba
dudwns0911 <dudwns0911@naver.com>
2020-05-03 13:20:41 +0900

    recycleviewexample

--- app/src/main/java/com/example/recyclerviewexample/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewexample/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.recyclerviewexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<Movie> list = new ArrayList<>();
-        for(int i = 0; i < 100; i++){
-            list.add(new Movie(getDrawable(R.drawable.ic_launcher_background),"hello"));
-        }
+        list.add(new Movie(getDrawable(R.drawable.image1), "영화1"));
+        list.add(new Movie(getDrawable(R.drawable.image2), "영화2"));
+        list.add(new Movie(getDrawable(R.drawable.image3), "영화3"));
+        list.add(new Movie(getDrawable(R.drawable.image4), "영화4"));
+        list.add(new Movie(getDrawable(R.drawable.image5), "영화5"));
+        list.add(new Movie(getDrawable(R.drawable.image6), "영화6"));
+        list.add(new Movie(getDrawable(R.drawable.image7), "영화7"));
+        list.add(new Movie(getDrawable(R.drawable.image8), "영화8"));
+        list.add(new Movie(getDrawable(R.drawable.image9), "영화9"));
+        list.add(new Movie(getDrawable(R.drawable.image10), "영화10"));
+
 
         RecyclerView recyclerView = findViewById(R.id.recycler1);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         MyAdapter adapter =new MyAdapter(list);
         recyclerView.setAdapter(adapter);
     }
